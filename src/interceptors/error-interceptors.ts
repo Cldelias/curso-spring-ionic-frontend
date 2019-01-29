eaabc4e84dfc9e5f7bd408a1e132bd047d7d88ac
Clodoaldo Elias <clodoaldoelias@gmail.com>
2019-01-29 21:37:11 -0200

    Tratando erros 403

--- src/interceptors/error-interceptors.ts
+++ src/interceptors/error-interceptors.ts
@@ -1,9 +1,15 @@
+import { StorageService } from './../services/storage.service';
+import { StatusBar } from '@ionic-native/status-bar';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class ErrorInterceptors implements HttpInterceptor {
+
+    constructor(public storage: StorageService) {
+
+    }
 
     intercept(req: HttpRequest<any>, next: HttpHandler) : Observable<HttpEvent<any>> {
         return next.handle(req)
@@ -17,8 +23,18 @@ export class ErrorInterceptors implements HttpInterceptor {
             }
             console.log("Erro detectado pelo interceptor");
             console.log(errorObj);
+
+            switch(errorObj.Status) {
+                case 403:
+                this.handle403();
+                break;
+            }
             return Observable.throw(errorObj);
         }) as any;
+    }
+
+    handle403() {
+        this.storage.setLocalUser(null);
     }
 }
 
